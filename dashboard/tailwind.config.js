05de0c7d14e3ae8809e99c9786a1de21f914e6fb
Jason Le <jason.le@sweat.com>
2024-10-25 17:13:27 +1030

    include this to be able to run tailwind css with flowbite

--- dashboard/tailwind.config.js
+++ dashboard/tailwind.config.js
@@ -4,7 +4,10 @@ import flowbitePlugin from 'flowbite/plugin'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		'./node_modules/flowbite/**/*.js'
+	],
 
 	theme: {
 		extend: {
